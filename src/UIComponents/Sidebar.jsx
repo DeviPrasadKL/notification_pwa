eb9eb21baa56cfaf5d82b3e56b486c5b30ab9485
deviprasadkl <deviprasadofficial@gmail.com>
2024-08-18 16:26:32 +0530

    fix: width adjust

--- src/UIComponents/Sidebar.jsx
+++ src/UIComponents/Sidebar.jsx
@@ -3,29 +3,37 @@ import { Drawer, List, ListItem, ListItemText, Divider } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 const Sidebar = ({ open, onClose }) => {
-
   const navigate = useNavigate();
 
   const navigateTo = (path) => {
     onClose();
     navigate(`${path}`);
-  }
+  };
 
   return (
     <Drawer
       anchor="left"
       open={open}
       onClose={onClose}
+      sx={{
+        width: '40%', // Default width for larger screens
+        flexShrink: 0,
+        '@media (max-width:600px)': {
+          width: '60%', // Adjust width for smaller screens if needed
+        }
+      }}
+      PaperProps={{ sx: { width: '40%' } }} // Set width of the Drawer Paper component
     >
       <List>
-        <ListItem button>
-          <ListItemText primary="Home" onClick={() => { navigateTo('/') }} />
+        <ListItem button onClick={() => { navigateTo('/') }}>
+          <ListItemText primary="Home" />
         </ListItem>
         <Divider />
-        <ListItem button>
-          <ListItemText primary="View History" onClick={() => { navigateTo('/view_history') }} />
+        <ListItem button onClick={() => { navigateTo('/view_history') }}>
+          <ListItemText primary="View History" />
         </ListItem>
         <Divider />
+        {/* Uncomment and add additional ListItems as needed */}
         {/* <ListItem button>
           <ListItemText primary="About" />
         </ListItem>
