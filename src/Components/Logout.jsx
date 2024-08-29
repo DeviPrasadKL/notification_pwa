53a88b862b9e490bccbc5b1fbdcf094f42a742a3
deviprasadkl <deviprasadofficial@gmail.com>
2024-08-29 22:30:58 +0530

    ref: code spliting

--- src/Components/Logout.jsx
+++ src/Components/Logout.jsx
@@ -5,9 +5,9 @@ const SettingsSection = Loadable(lazy(() => import('./SettingsSection')));
 const CloseConfirm = Loadable(lazy(() => import('../UIComponents/ConfirmationDialog')));
 const LogoutAndCalculate = Loadable(lazy(() => import('./LogoutAndCalculate')));
 const RecordExistsConfirm = Loadable(lazy(() => import('../UIComponents/RecordExistsConfirm')));
+const BreaksTable = Loadable(lazy(() => import('./BreaksTable')));
 
-import { Button, Container, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Stack, TextField, IconButton, Box } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
+import { Button, Container, Typography, Stack, TextField, IconButton, Box } from '@mui/material';
 import SettingsIcon from '@mui/icons-material/Settings';
 import useTotalLoggedInHours from '../CustomHooks/useTotalLoggedInHours';
 import EditIcon from '@mui/icons-material/Edit';
@@ -636,40 +636,13 @@ export default function Logout({ darkMode, handleThemeToggle }) {
             </Stack>
 
             {breaks.length !== 0 && (
-                <TableContainer component={Paper} style={{ marginTop: 20 }}>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Start</TableCell>
-                                <TableCell>End</TableCell>
-                                <TableCell>Duration</TableCell>
-                                <TableCell>Actions</TableCell> {/* New column for delete icon */}
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {breaks.map((breakRecord, index) => (
-                                <TableRow key={index}>
-                                    <TableCell>{new Date(breakRecord.start).toLocaleTimeString('en-US', timeOptions)}</TableCell>
-                                    <TableCell>{new Date(breakRecord.end).toLocaleTimeString('en-US', timeOptions)}</TableCell>
-                                    <TableCell>{breakRecord.duration}</TableCell>
-                                    <TableCell>
-                                        <IconButton
-                                            onClick={() => handleDeleteBreak(index)}
-                                            color="error"
-                                            disabled={!canDeleteBreak(breakRecord.end)}
-                                        >
-                                            <DeleteIcon />
-                                        </IconButton>
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                            <TableRow>
-                                <TableCell colSpan={2} style={{ fontWeight: 'bold' }}>Total Break Duration</TableCell>
-                                <TableCell colSpan={2} style={{ fontWeight: 'bold' }}>{calculateTotalBreakDuration()}</TableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                <BreaksTable
+                breaks={breaks}
+                handleDeleteBreak={handleDeleteBreak}
+                canDeleteBreak={canDeleteBreak}
+                calculateTotalBreakDuration={calculateTotalBreakDuration}
+                timeOptions={timeOptions}
+                />
             )}
 
             {showAddBreakManually && (
